846675d99b475ce5583dddf307572bd44dbe9baa
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-10-30 15:56:35 +0200

    part1 initial changes

--- Article/Article.js
+++ Article/Article.js
@@ -112,7 +112,7 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
-  function createArticle (articleObject) {
+  function articleCreator (articleObject) {
 
     const article = document.createElement('div');
     const articleTitle = document.createElement('h2');
@@ -149,5 +149,8 @@ const data = [
   div.append(para3);
   div.append(span);
 
-  
+articleCreator.forEch(article => {
+  console.log('Create an article . . .', article.title);
+  article.append(articleCreator(articleObject));
+});
 
